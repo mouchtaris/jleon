26302aebb157feadcabfeda22bb454cbd2e761a8
Nikos Mouchtaris <nikos.mouchtaris@redtulipsystems.com>
2017-05-22 22:39:52 +0000

    Add FetchManager interpretations

--- src/main/scala-2.12/gv/jleon/domain/FetchManager.scala
+++ src/main/scala-2.12/gv/jleon/domain/FetchManager.scala
@@ -1,11 +1,13 @@
 package gv.jleon
 package domain
 
+import scala.language.{ implicitConversions }
+
 import akka.{ NotUsed }
 import akka.util.{ ByteString }
 import akka.stream.scaladsl.{ Source }
 
-import shapeless.{ HNil, :: }
+import shapeless.{ HNil, ::, HList, Generic }
 
 trait FetchManager extends Any {
   def mirrors: MirrorRepository
@@ -40,4 +42,36 @@ trait FetchManager extends Any {
 
 object FetchManager {
 
+  trait Interpretation[T] extends Any {
+    final type Self = T
+
+    def mirrorsFrom(self: Self): MirrorRepository
+    def storageFrom(self: Self): LockingStorage
+  }
+
+  final type Record1[Rest <: HList] = MirrorRepository :: LockingStorage :: Rest
+
+  final implicit def recordI[Rest <: HList]: Interpretation[Record1[Rest]] = new Interpretation[Record1[Rest]] {
+    override def mirrorsFrom(self: Self): MirrorRepository = self match {
+      case mirrors :: _ ⇒ mirrors
+    }
+    override def storageFrom(self: Self): LockingStorage = self match {
+      case _ :: storage :: _ ⇒ storage
+    }
+  }
+
+  final case class ADT(
+    override val mirrors: MirrorRepository,
+    override val storage: LockingStorage
+  ) extends FetchManager
+
+  final implicit def genericI[T, Repr](implicit generic: Generic.Aux[T, Repr], i: Interpretation[Repr]) = new Interpretation[T] {
+    override def mirrorsFrom(self: Self): MirrorRepository = i mirrorsFrom generic.to(self)
+    override def storageFrom(self: Self): LockingStorage = i storageFrom generic.to(self)
+  }
+
+  final implicit def apply[T](self: T)(implicit i: Interpretation[T]): FetchManager = new FetchManager {
+    override def mirrors: MirrorRepository = i mirrorsFrom self
+    override def storage: LockingStorage = i storageFrom self
+  }
 }
