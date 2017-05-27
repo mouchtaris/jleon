45f68296506854772e5ee8459745a024fbbfd5e0
Nikos Mouchtaris <nikos.mouchtaris@redtulipsystems.com>
2017-05-27 19:14:49 +0000

    Scalariform

--- src/main/scala-2.12/gv/isi/convertible/package.scala
+++ src/main/scala-2.12/gv/isi/convertible/package.scala
@@ -5,12 +5,12 @@ package object convertible extends AnyRef
 
   implicit class Conversion[A, B](override val self: A ⇒ B)
     extends AnyVal
-      with ConversionOps.Ops[A, B]
+    with ConversionOps.Ops[A, B]
 
   final type ~⇒[A, B] = Conversion[A, B]
 
   implicit class Convertible[S](override val self: S)
-      extends AnyVal
-      with ConvertibleOps.Ops[S]
+    extends AnyVal
+    with ConvertibleOps.Ops[S]
 
 }
--- src/main/scala-2.12/gv/isi/functional/TupledCall.scala
+++ src/main/scala-2.12/gv/isi/functional/TupledCall.scala
@@ -1,26 +1,26 @@
 package gv.isi.functional
 
 /**
-  * Allow the following trick:
-  * {{{
-  *   trait F[T] {
-  *     type R
-  *     def f(self: T): R
-  *   }
-  *
-  *   implicit final object FInt extends F[Int] {
-  *     type R = Int
-  *     def f(self: Int): Int = self + 1
-  *   }
-  *
-  *   val bundle = (FInt, 12)
-  *
-  *   println { // Prints 13
-  *     bundle method (_.f)
-  *   }
-  *
-  * }}}
-  */
+ * Allow the following trick:
+ * {{{
+ *   trait F[T] {
+ *     type R
+ *     def f(self: T): R
+ *   }
+ *
+ *   implicit final object FInt extends F[Int] {
+ *     type R = Int
+ *     def f(self: Int): Int = self + 1
+ *   }
+ *
+ *   val bundle = (FInt, 12)
+ *
+ *   println { // Prints 13
+ *     bundle method (_.f)
+ *   }
+ *
+ * }}}
+ */
 object TupledCall {
 
   final implicit class Decoration[I, S](val interpSelf: (I, S)) extends AnyVal {
